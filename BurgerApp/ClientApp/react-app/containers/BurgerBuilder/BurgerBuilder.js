63b53dba80d8cf0276e37c66ac85d22a59da4178
UltimaBeaR <thebear@yandex.ru>
2017-12-10 19:22:14 +0300

    video #208 passed

--- BurgerApp/ClientApp/react-app/containers/BurgerBuilder/BurgerBuilder.js
+++ BurgerApp/ClientApp/react-app/containers/BurgerBuilder/BurgerBuilder.js
@@ -96,35 +96,12 @@ class BurgerBuilder extends Component {
     purchaseContinueHandler = () => {
         // alert('You continue!');
 
-        // this.setState({ loading: true });
-        
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: 'Ilya Mikhalin',
-        //         address: {
-        //             street: 'Teststreet 1',
-        //             zipCode: '41351',
-        //             country: 'Russia'
-        //         },
-        //         email: 'test@test.com'
-        //     },
-        //     deliveryMethod: 'fastest'
-        // };
-
-        // axios.post('/orders.json', order)
-        //     .then(response => {
-        //         this.setState({ loading: false, purchasing: false });
-        //     })
-        //     .catch(error => {
-        //         this.setState({ loading: false, purchasing: false });
-        //     });
-
         const queryParams = [];
         for (let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
+
+        queryParams.push('price=' + this.state.totalPrice);
 
         const queryString = queryParams.join('&');
 
