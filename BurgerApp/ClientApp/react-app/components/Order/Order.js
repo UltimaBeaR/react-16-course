9aa71b1bf27ea34b308440e3bdfb264de0f6b71b
UltimaBeaR <thebear@yandex.ru>
2017-12-10 20:23:58 +0300

    video #213 passed (chapter finished)

--- BurgerApp/ClientApp/react-app/components/Order/Order.js
+++ BurgerApp/ClientApp/react-app/components/Order/Order.js
@@ -2,11 +2,41 @@ import classes from './Order.css';
 
 import React from 'react';
 
-const order = () => (
-    <div className={classes.Order}>
-        <p>Ingredients: Salad (1)</p>
-        <p>Price: <strong>USD 5.45</strong></p>
-    </div>
-);
+const order = ({ ingredients, price }) => {
+    const newIngredients = [];
+    
+    for (let ingredientName in ingredients) {
+        newIngredients.push(
+            {
+                name: ingredientName,
+                amount: ingredients[ingredientName]
+            }
+        );
+    }
+
+    const ingredientOutput = newIngredients.map(ig => {
+        return (
+            <span
+                style={{
+                    textTransform: 'capitalize',
+                    display: 'inline-block',
+                    margin: '0 8px',
+                    border: '1px solid #ccc',
+                    padding: '5px'
+                }}
+                key={ig.name}
+            >
+                {ig.name} ({ig.amount})
+            </span>
+        );
+    });
+
+    return (
+        <div className={classes.Order}>
+            <p>Ingredients: {ingredientOutput}</p>
+            <p>Price: <strong>USD {Number.parseFloat(price).toFixed(2)}</strong></p>
+        </div>
+    );
+};
 
 export default order;
